148ba63000ecfd0be172ee4db3b0c5a88de8abde
Kiran Jayarama <kiran760043@.github.com>
2020-07-07 15:14:37 -0400

    Update baud_rate.v

--- baud_rate.v
+++ baud_rate.v
@@ -9,17 +9,17 @@ module baud_rate(clk, rst, baud_clk);
 
     input  wire clk;
     input  wire rst;
-    output wire baud_clk;
-
+    output reg  baud_clk;
+    
     reg [13:0] count;
+    
+//    reg [13:0] br = 14'h28b4;            //9600
+    reg [13:0] br = 14'h37F;           //115200
 
-    reg [13:0] br = 14'h28b4;            //9600
-//    reg [13:0] br = 14'h37F;           //115200
-
-    always@(posedge clk)
+    always@(posedge clk or posedge rst)
         begin
             if(rst) begin
-                count = 0;
+                count = 0;    
             end else begin
                 if(count == br)
                     count = 0;
@@ -27,7 +27,13 @@ module baud_rate(clk, rst, baud_clk);
                     count = count + 1;
             end
         end
-
-    assign baud_clk =(count < br/2) ? 1'b0 : 1'b1;
-
+    
+    always@(posedge clk)
+        begin
+            if(count < br/2) 
+                baud_clk <= 0;
+            else
+                baud_clk <= 1;
+        end
+     
 endmodule
